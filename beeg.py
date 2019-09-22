73266cc864b36a04cea9e4b43946c5e830b33aee
izag <zzz>
2019-09-22 05:21:18 +0500

    Remove from favorites

--- beeg.py
+++ beeg.py
@@ -74,12 +74,15 @@ class MainWindow:
         self.cb_model.focus_set()
         self.cb_model.grid(row=self.level, column=0, columnspan=4, sticky=W + E, padx=PAD, pady=PAD)
 
+        self.btn_remove = Button(root, text="-", command=self.remove_from_favorites)
+        self.btn_remove.grid(row=self.level, column=4, sticky=W + E, padx=PAD, pady=PAD)
+
         self.level += 1
         self.btn_update = Button(root, text="Update info", command=lambda: self.update_model_info(True))
         self.btn_update.grid(row=self.level, column=0, sticky=W + E, padx=PAD, pady=PAD)
 
         self.cb_resolutions = ttk.Combobox(root, state="readonly", values=[])
-        self.cb_resolutions.grid(row=self.level, column=1, columnspan=3, sticky=W + E, padx=PAD, pady=PAD)
+        self.cb_resolutions.grid(row=self.level, column=1, columnspan=4, sticky=W + E, padx=PAD, pady=PAD)
 
         self.level += 1
         self.btn_show_recording = Button(root,
@@ -96,7 +99,7 @@ class MainWindow:
         self.chk_use_proxy.grid(row=self.level, column=1, sticky=W, padx=PAD, pady=PAD)
 
         self.cb_proxy = ttk.Combobox(root, width=30, state=DISABLED)
-        self.cb_proxy.grid(row=self.level, column=2, columnspan=2, sticky=W + E, padx=PAD, pady=PAD)
+        self.cb_proxy.grid(row=self.level, column=2, columnspan=3, sticky=W + E, padx=PAD, pady=PAD)
 
         self.level += 1
         self.btn_start = Button(root, text="Start", command=self.on_btn_start)
@@ -109,7 +112,7 @@ class MainWindow:
         self.copy_button.grid(row=self.level, column=2, sticky=W + E, padx=PAD, pady=PAD)
 
         self.paste_button = Button(root, text="Paste", command=self.paste_model_name)
-        self.paste_button.grid(row=self.level, column=3, sticky=W + E, padx=PAD, pady=PAD)
+        self.paste_button.grid(row=self.level, column=3, columnspan=2, sticky=W + E, padx=PAD, pady=PAD)
 
         self.level += 1
         self.progress = ttk.Progressbar(root, orient=HORIZONTAL, length=120, mode='indeterminate')
@@ -173,7 +176,7 @@ class MainWindow:
 
         self.btn_stop.config(state=NORMAL)
         self.btn_show_recording.config(state=NORMAL)
-        self.progress.grid(row=self.level, column=0, columnspan=4, sticky=W + E, padx=PAD, pady=PAD)
+        self.progress.grid(row=self.level, column=0, columnspan=5, sticky=W + E, padx=PAD, pady=PAD)
         self.progress.start()
 
         self.update_title()
@@ -256,14 +259,25 @@ class MainWindow:
         return True
 
     def add_to_history(self, name):
-        if len(self.cb_model['values']) == 0:
-            self.cb_model['values'] = name
-        elif name not in self.cb_model['values']:
+        if name not in self.cb_model['values']:
             self.cb_model['values'] = (name, *self.cb_model['values'])
 
         self.hist_logger.info(name)
         count = self.hist_dict.get(name, 0)
         self.hist_dict[name] = count + 1
+
+    def remove_from_favorites(self):
+        name = self.cb_model.get().strip()
+        values = list(self.cb_model['values'])
+        if name not in values:
+            return
+
+        values.remove(name)
+        self.cb_model['values'] = tuple(values)
+        if len(values) == 0:
+            self.cb_model.set('')
+        else:
+            self.cb_model.set(values[0])
 
     def add_to_proxies(self, proxy):
         if len(self.cb_proxy['values']) == 0:
@@ -415,7 +429,7 @@ class MainWindow:
             self.show_image = False
         else:
             self.show_image = True
-            self.image_label.grid(row=0, column=0, columnspan=4, sticky=W + E, padx=PAD, pady=PAD)
+            self.image_label.grid(row=0, column=0, columnspan=5, sticky=W + E, padx=PAD, pady=PAD)
             self.update_model_info(True)
 
     def show_full_history(self):
