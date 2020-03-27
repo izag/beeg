dd6a2c1ed5ef7a8a08dacbffa5e292e08faaddd5
izag <izag@splinex.com>
2020-03-27 05:09:42 +0500

    Enhance merge script.

--- mrg.bat
+++ mrg.bat
@@ -1,7 +1,13 @@
 @Echo OFF
+setlocal EnableDelayedExpansion
 
 FOR /D /R %%# in (*) DO (
 	PUSHD "%%#"
+	set suffix=1000000
+	for /F "delims=" %%i in ('dir /B *.ts') do (
+	   set /A suffix+=1
+	   ren "%%i" "!suffix:~1!.ts"
+	)
 	copy /b *ts all.xts
 	del *.ts
 	move all.xts all.ts
